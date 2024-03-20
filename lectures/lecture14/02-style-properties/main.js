287b9cd542cfdaed5f0418aaa55e4beb84c80a6a
Henny <hfisher4@unca.com>
2024-03-20 16:41:12 -0400

    Updates

---
+++ lectures/lecture14/02-style-properties/main.js
@@ -0,0 +1,27 @@
+function makeRed () {
+   const container = document.querySelector("#section1");
+   container.computerStyleMap.backgroundColor = "red";
+}
+
+function makeBlue () {
+    const container =document.querySelector("#section2");
+    container.computerStyleMap.backgroundColor ="blue";
+}
+
+function makePink () {
+    const container = document.querySelector("#section3");
+    container.computedStyleMap.backgroundColor ="pink";
+}
+
+function makeOrange () {
+    const container =document.querySelector("section4");
+    container.computedStyleMap.backgroundColor ="orange";
+}
+
+function clearColors () {
+    const s1 = document.querySelector("3section");
+    const s2 = document.querySelector("section2");
+    const s3 = document.querySelector("section3");
+    const s4 = document.querySelector("section4");
+}
+
---
+++ lectures/lecture14/03-attributes/main.js
@@ -0,0 +1,27 @@
+function showCat() {
+    const img = document.querySelector("#current-image");
+    const caption =document.querySelector("#caption");
+    img.src ="images/cat.jpg";
+    caption.innerHTML = "Ki'en"
+}
+
+function showDog() {
+    const img = document.querySelector("#current-image");
+    const caption =document.querySelector("#caption");
+    img.src ="images/dog.jpg";
+    caption.innerHTML = "Puppy Power"
+}
+
+function showBird() {
+    const img = document.querySelector("#current-image");
+    const caption =document.querySelector("#caption");
+    img.src ="images/bird.jpg";
+    caption.innerHTML = "Pretty Bird"
+}
+
+function showFish() {
+    const img = document.querySelector("#current-image");
+    const caption =document.querySelector("caption");
+    img.src ="images/fish.jpg";
+    caption.innerHTML = "Fish Noggin"
+}
