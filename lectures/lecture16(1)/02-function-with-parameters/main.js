22ec2a4bfb958f9b0c8914df5e46a9b7c83e3957
Henny <hfisher4@unca.com>
2024-03-29 12:14:51 -0400

    tutorial 8

--- lectures/lecture16(1)/02-function-with-parameters/main.js
+++ lectures/lecture16(1)/02-function-with-parameters/main.js
@@ -1,1 +1,22 @@
 // make a function:
+
+function changeColor(color) {
+    const bodyEl = document.querySelector("body");
+    bodyEl.style.backgroundColor = color;
+}
+
+//function makeRed() {
+    // target the body and change background color to red:
+    //document.querySelector("body").style.backgroundColor ="#B23";
+
+//}
+
+//function makeBlue() {
+    //const bodyEl = document.querySelector("body");
+    //bodyEl.style.backgroundColor = "#2af";
+//}
+
+//function makeGreen() {
+    //const bodyEl = document.querySelector("body");
+    //bodyEl.style.backgroundColor = "#3a3";
+//}
--- lectures/lecture16(1)/03-drawings/sketch.js
+++ lectures/lecture16(1)/03-drawings/sketch.js
@@ -13,32 +13,33 @@ function setup() {
 
     // https://p5js.org/reference/#/p5/circle
     fill("black");
-    circle(150, 100, 400);
+    circle(150, 300, 300);
 
-    // https://p5js.org/reference/#/p5/rect
-    fill('white');
-    circle(100, 150, 80);
-    circle(200, 200, 55);
 
-    fill("black");
-    circle(100, 150, 80);
-    circle(200, 100, 55);
+    
 
 
     // add your drawing here:
     // https://p5js.org/reference/#/p5/point
     // point(110, 20);
     strokeWeight (20);
-    point (110,20);
+    point (110, 20);
 
     // https://p5js.org/reference/#/p5/text
-    fill ("black");
-    text ("Here is some text", 200, 40);
+
     // text("Here is some text", 200, 40);
 
     // https://p5js.org/reference/#/p5/ellipse
     // fill('red');
     // ellipse(100, 200, 60, 100);
+    fill ("white");
+    strokeWeight (1);
+    ellipse (100, 200, 60, 100);
+    ellipse(200, 200, 60, 100);
+    fill("black");
+    circle(100, 200, 40, 40);
+    circle(200, 200, 40, 40);
+    
 
     // Other shapes...
     // Polygon: https://p5js.org/reference/#/p5/beginShape
