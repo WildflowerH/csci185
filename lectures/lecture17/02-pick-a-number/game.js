e163d8b3be54d43a90ce95fe3ad4b462216d009b
Henny <hfisher4@unca.com>
2024-04-01 15:40:50 -0400

    project and lecture updates

--- lectures/lecture17/02-pick-a-number/game.js
+++ lectures/lecture17/02-pick-a-number/game.js
@@ -7,7 +7,35 @@ function generateRandomNumber(low, high) {
 const secret = generateRandomNumber(1, 100);
 console.log(secret);
 
+let numGuesses = 0;
+
 // Your task:
 function check() {
-    // your code here
+
+    //how do we set the guess variable?
+    const guess = Number(document.querySelector ("#guess").value);
+    const messageEl = document.querySelector("#message");
+    const numGuessesEl = document.querySelector("#num_guesses");
+
+    // your code here //how do you know if the guess is too high?
+    if (guess > secret) {
+       // console.log ("too high");
+       messageEl.innerHTML ="Too high!";
+
+    }
+    if (guess < secret) {
+        // console.log ("too low");
+        messageEl.innerHTML ="Too high!";
+ 
+     }
+
+    if (guess === secret) {
+        //console.log("You win!");
+        messageEl.innerHTML ="You Win!!!!";
+    
+    }
+    numGuesses = numGuesses + 1; 
+    numGuessesEl.innerHTML = numGuesses;
+
+
 }
--- lectures/lecture17/03-game-controller/game.js
+++ lectures/lecture17/03-game-controller/game.js
@@ -16,12 +16,37 @@ function drawPlayer() {
     clear();
     fill(color);
     circle(x, y, width); // x position, y position, diameter
-    drawGrid(window.innerWidth, window.innerHeight);
+    //drawGrid(window.innerWidth, window.innerHeight);
 }
 
 function movePlayer(ev) {
     // open the console in your browser to see what happens :)
     console.log(ev.code);
+    if (ev.code ==="ArrowUp") {
+        y = y - 5;
+    }
+    else if (ev.code === "ArrowDown") {
+        y = y + 5;
+    }
+    else if (ev.code === "ArrowRight") {
+        x = x + 5;
+    }
+    else if (ev.code === "ArrowLeft") {
+        x = x - 5;
+    }
+    else if ( ev.code === "Space") {
+        width = width + 5;
+    }
+    else if (ev.code ==="Escape") {
+        width = width - 5;
+    }
+    else if (ev.code ==="KeyB") {
+        color = "blue";
+    }
+    else if (ev.code ==="KeyR") {
+        color = "red";
+    }
+
 
     // re-draw the player
     drawPlayer();
