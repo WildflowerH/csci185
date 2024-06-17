904c620a940a34761801464a663922da4e38ec3a
Henny <hfisher4@unca.com>
2024-06-17 16:37:36 -0400

    HW6

--- tutorials/hw06/your_task/main.js
+++ tutorials/hw06/your_task/main.js
@@ -12,11 +12,48 @@ function search (ev) {
     }
 }
 
+function playTrack(trackId) {
+    const template =
+    <iframe
+    style="border-radius:12px"
+    src="https://open.spotify.com/embed/track/${trackId}?utm_source=generator" 
+    width="100%" 
+    height="352" 
+    frameBorder="0" 
+    allowfullscreen="" 
+    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+    loading="lazy">
+
+    </iframe>;
+
+
+    document.querySelector("#artist").innerHTML = template;
+}
+
 async function getTracks (term) {
-    console.log(`
-        get tracks from spotify based on the search term
-        "${term}" and load them into the #tracks section 
-        of the DOM...`);
+    //query for tracks
+    const url = `https://www.apitutor.org/spotify/simple/v1/search?type=track&q=${term}`;
+    const response = await fetch(url);
+    const trackData = await response.json();
+    console.log(trackData);
+
+    //clear the previous search
+    document.querySelector("#tracks").innerHTML ="";
+
+    //loop through track data and display 1st 5tracks
+    for (let i = 0; i < 5; i++){
+    const firstTrack = trackData(i);
+    const template =
+     `<section class="track-item preview"> onclick="playTrack('${track.id}')">
+    <img src="${track.album.image_url}">
+    <i class="fas play-track fa-play" aria-hidden="true"></i>
+    <div class="label">
+        <h2>${track.name}</h2>
+        <p>${track.artist.name}</p>
+    </div>
+</section>
+`;
+document.querySelector("$tracks").innerHTML += template
 }
 
 async function getAlbums (term) {
@@ -27,11 +64,33 @@ async function getAlbums (term) {
 }
 
 async function getArtist (term) {
-    console.log(`
-        get artists from spotify based on the search term
-        "${term}" and load the first artist into the #artist section 
-        of the DOM...`);
-};
+    //query spotify for artist
+    const url = `https://www.apitutor.org/spotify/simple/v1/search?type=artist&q=$(term)`;
+    const response = await fetch(url);
+    const artistData = await response.json();
+    console.log(artistData);
+    const artist = artistData(0);
+
+    //update template with our artist
+
+
+
+    const template = `
+    <section class="artist-card" id="${artist.id}">
+    <div>
+        <img src="${artist.image_url}">
+        <h2>${artist.name}</h2>
+        <div class="footer">
+            <a href="${artist.spotify_url}" target="_blank">
+                view on spotify
+            </a>
+        </div>
+    </div>
+</section>
+`;
+document.querySelector('#artist').innerHTML = template;
+}
+
 
 
 document.querySelector('#search').onkeyup = function (ev) {
