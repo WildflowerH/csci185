7f7cb6c51e17e7d7530cd8bd7af42e6be18ce2c0
Hendrix <hfisher4@unca.edu>
2024-07-23 12:55:24 -0400

    still working

---
+++ exercise01/main.js
@@ -0,0 +1,18 @@
+function makeBlue() {
+    // your code here
+    
+    console.log('change the background color to blue');
+    document.querySelector("#s1").style.backgroundColor="blue";
+}
+
+function goodbye() {
+    // your code here
+
+    console.log('change message to Goodbye');
+    document.querySelector("message").className="Goodbye";
+    messageElement.textContent="Goodbye";
+    
+}
+
+function showDog() {
+    // your code here  
